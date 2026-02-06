85771518f0cf962d4781675a192b0992f58ef073
SRIRAM <94049113+SRIRAM6369791334@users.noreply.github.com>
2026-02-06 11:50:01 +0530

    Added dashboard route to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
